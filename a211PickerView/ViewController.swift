29b0647295e121b50ad51761dd162fcdabdaae9a
Shen Jun Wu <shenfive@gmail.com>
2020-11-21 16:24:11 +0800

    data Source

--- a211PickerView/ViewController.swift
+++ a211PickerView/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDataSource {
 
+    
+
+    
+
+    @IBOutlet weak var pickerView: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        pickerView.dataSource = self
+        
+        
+        
     }
 
-
+    //MARK: pickerView DataSource
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+        case 0:
+            return 5
+        case 1:
+            return 8
+        default:
+            return 0
+        }
+    }
+    
+    
+    
 }
 
