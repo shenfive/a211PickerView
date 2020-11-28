405a8792b91d84d52faba7b5c17c21ea55908e52
Shen Jun Wu <shenfive@gmail.com>
2020-11-28 10:23:31 +0800

    page2

---
+++ a211PickerView/Page2ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  Page2ViewController.swift
+//  a211PickerView
+//
+//  Created by 申潤五 on 2020/11/28.
+//
+
+import UIKit
+
+class Page2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("page2")
+    }
+    
+
+}
