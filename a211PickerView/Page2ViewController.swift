61714549afcae3744d431ca7c7d03243a0308dbd
Shen Jun Wu <shenfive@gmail.com>
2020-11-28 10:39:30 +0800

    準備下一頁

--- a211PickerView/Page2ViewController.swift
+++ a211PickerView/Page2ViewController.swift
@@ -9,10 +9,21 @@ import UIKit
 
 class Page2ViewController: UIViewController {
 
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    
+    var bld = ""
+    var str = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         print("page2")
+        label1.text = bld
+        label2.text = str
+        
+        
+        
     }
     
 
